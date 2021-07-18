d813f05dae471c11ff4f56318de631c6e62fe43c
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-07-17 22:51:53 -0400

    have to adjust task fragment so menu doesnt block recycler view

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/Fragments/Task/TaskFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/Fragments/Task/TaskFragment.kt
@@ -1,11 +1,10 @@
 package com.codinginflow.mvvmtodo.ui.Fragments.Task
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.codinginflow.mvvmtodo.R
 import com.codinginflow.mvvmtodo.databinding.FragmentTaskBinding
 import com.codinginflow.mvvmtodo.ui.viewmodel.TaskViewModel
+import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,6 +46,43 @@ class TaskFragment : Fragment() {
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
+        setHasOptionsMenu(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.onQueryTextChanged {
+            // update search query
+            viewModel.searchQuery.value = it
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/viewmodel/TaskViewModel.kt
@@ -5,12 +5,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.database.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class TaskViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
-    val tasks = taskDao.getTasks().asLiveData()
+
+    val searchQuery = MutableStateFlow("")
+
+    private val taskFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = taskFlow.asLiveData()
 
 }
