82d2f01190406cbd2b4248444df789cd555178e2
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-07-18 17:15:23 -0400

    forgot to add reading the stored data from datastore into fragment with coroutine

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/Fragments/Task/TaskFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/Fragments/Task/TaskFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.widget.LinearLayout
 import android.widget.SearchView
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
@@ -15,6 +16,9 @@ import com.codinginflow.mvvmtodo.databinding.FragmentTaskBinding
 import com.codinginflow.mvvmtodo.ui.viewmodel.TaskViewModel
 import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TaskFragment : Fragment() {
@@ -58,7 +62,10 @@ class TaskFragment : Fragment() {
         searchView.onQueryTextChanged {
             // update search query
             viewModel.searchQuery.value = it
-
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
         }
     }
 
